4e897228e4ae10ca748d12a96bc42a3feece9225
javier.varela <javier.varela@udla.edu.ec>
2023-11-04 10:13:57 -0500

    Migracion para actualizar modelo producto

--- AirsoftCore.Data/Migrations/20231104151234_ActualizacionModeloProducto.Designer.cs
+++ AirsoftCore.Data/Migrations/20231104151234_ActualizacionModeloProducto.Designer.cs
@@ -4,6 +4,7 @@ using AirsoftCore.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AirsoftCore.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231104151234_ActualizacionModeloProducto")]
+    partial class ActualizacionModeloProducto
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AirsoftCore.Data/Migrations/20231104151234_ActualizacionModeloProducto.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AirsoftCore.Data.Migrations
+{
+    public partial class ActualizacionModeloProducto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Precio",
+                table: "Productos",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Precio",
+                table: "Productos");
+        }
+    }
+}
